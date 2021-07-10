fdd8ebb67a54944f7d732abb81c19c3e2fabbfc6
lyfewithcode <lyfewithcode@gmail.com>
2021-07-10 10:13:00 +0700

    Jquery selectors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// for syntax class
+
+/*
 $(document).ready(function() {
     // $("p:first-child").hide(5000);
     $("p").click(function() {
@@ -6,8 +9,22 @@ $(document).ready(function() {
     
     });
 });
-
+*/
 
 // $ - it is used to access jquery
 // selector - html selector
 // action
+
+// for selector class, starting here
+
+$(document).ready(function() {
+    // $("p:first-child").hide(5000);
+    $("h1").click(function() {
+
+        $(this).hide(2000);
+    
+    });
+
+    $(".first-paragraph").hide(1000);
+    $("#me").hide(1000);
+});

9f436ed02cded0a382c5a967149c3b6a6be25527
lyfewithcode <lyfewithcode@gmail.com>
2021-07-10 09:54:07 +0700

    Jquery syntax

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // $("p:first-child").hide(5000);
+    $("p").click(function() {
+
+        $(this).hide(2000);
+    
+    });
+});
+
+
+// $ - it is used to access jquery
+// selector - html selector
+// action
